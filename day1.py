8a2aa8b7499f0dd32dfa14843f59adc2be0c5293
gxm8595 <84980892+gxm8595@users.noreply.github.com>
2021-07-24 08:08:42 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,44 @@
+# print("Hello gaurav")
+
+# chapter 2= variables
+# first_name = 'Gaurav'
+# last_name = 'Sharma'
+# full_name = first_name + " " + last_name
+# print("Hello " + full_name)
+# print(type(name))
+
+# int data type
+# age = 19
+# age += 1 #age = age +1
+# print(age)
+# print(type(age))
+# print("Your age is: "+ str(age))
+
+# float data type
+# height = 180.5
+# print(height)
+# print(type(height))
+# print("Your height is: " +  str(height) +"cm")
+
+# bool data type
+# human = True
+# print(human)
+# print(type(human))
+# print("Are you human "+str(human))
+
+
+
+# Multiple assignment: allows us to assign multiple variables at the same time in one line of code
+# name = "Gaurav"
+# age = 19
+# Attractive = True
+#
+# print(name)
+# print(age)
+# print(Attractive)
+
+# name, age, attractive = 'Gaurav', 21, True
+#
+# print(name)
+# print(age)
+# print(attractive)
